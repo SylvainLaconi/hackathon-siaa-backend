4214e8bc6562725b2389bd834ca6ee9777801a28
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-06-29 01:26:49 +0200

    route DELETE post et GET by community

--- routes/post.js
+++ routes/post.js
@@ -15,6 +15,42 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get(
+  '/category/:cat&ampcommunity=:com1&ampcommunity=:com2&ampcommunity=:com3&ampcommunity=:com4',
+  (req, res) => {
+    connection.query(
+      'SELECT * from post WHERE post_category_id=? AND community_id IN (?, ?, ?, ?)',
+      [
+        req.params.cat,
+        req.params.com1,
+        req.params.com2,
+        req.params.com3,
+        req.params.com4,
+      ],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error retrieving data');
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  }
+);
+
+router.delete('/:id', (req, res) => {
+  const postId = req.params.id;
+  connection.query('DELETE FROM post WHERE id = ?', [postId], (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error deleting movie');
+    } else {
+      res.status(200).send('Post successfully deleted');
+    }
+  });
+});
+
 router.post('/', (req, res) => {
   const {
     title,
