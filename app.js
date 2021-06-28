0044b1147e7b4f8080ed60c82cea708d4a282cc6
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-06-28 19:28:49 +0200

    routes GET ok

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+
+const userRouter = require('./routes/user');
+const communityRouter = require('./routes/community');
+const postCategoryRouter = require('./routes/post_category');
+const postRouter = require('./routes/post');
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // TODO: configure cors
 app.use(cors());
 
 // TODO: add your routes here
+app.use('/api/user', userRouter);
+app.use('/api/community', communityRouter);
+app.use('/api/post_category', postCategoryRouter);
+app.use('/api/post', postRouter);
 
 module.exports = app;
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+/* eslint-disable camelcase */
+const express = require('express');
+const connection = require('../Config');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const sql = 'SELECT * FROM user';
+  connection.query(sql, (err, result) => {
+    if (err) {
+      res.status(500).json({ errorMessage: err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+module.exports = router;
