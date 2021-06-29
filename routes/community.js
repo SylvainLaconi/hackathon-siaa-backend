5ab3f225602c3866fb0f38c4aa31390f57a94512
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-06-29 16:55:06 +0200

    post new community to user

--- routes/community.js
+++ routes/community.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
 const express = require('express');
 const connection = require('../Config');
@@ -14,6 +15,29 @@ router.get('/', (req, res) => {
       res.status(200).json(result);
     }
   });
+});
+
+// Route pour s'inscrire à une communauté
+
+router.post('/', (req, res) => {
+  const { user_id, community_id } = req.body;
+  connection.query(
+    'INSERT INTO user_has_community (user_id, community_id) VALUES (?, ?)',
+    [user_id, community_id],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error adding new community');
+      } else {
+        const newCommunity = { user_id, community_id };
+        res
+          .status(201)
+          .send(
+            `${`User ${newCommunity.user_id} added community ${newCommunity.community_id}`} correctly`
+          );
+      }
+    }
+  );
 });
 
 module.exports = router;
